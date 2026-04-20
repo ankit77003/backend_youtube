a193e170f28f185bc00c7499d33a4d4e6dba3e17
ankit77003 <ankit77003@gmail.com>
2026-04-20 20:27:40 +0530

    added more schema model

---
+++ src/models/playlist.model.js
@@ -0,0 +1,22 @@
+import mongoose, { mongo } from "mongoose";
+
+const playlistSchema=new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    videos: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video"
+    }],
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+},{timestamps: true})
+
+export const Playlist=mongoose.model("Playlist", playlistSchema)
